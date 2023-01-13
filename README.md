ec9f292de34d9aeb8f4ddc6531a302f4744e9751
A-Rogachev <97498636+A-Rogachev@users.noreply.github.com>
2023-01-13 15:12:29 +0400

    added models and readme.MD

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# api_final
-api final
+<a id='start_page'></a>
+# <p align = center>API для Yatube</p>
+#### *Application programming interface (REST API) для проекта Yatube*
+___
+-   Возможность получать/редактировать публикации/комментарии к публикациям, получать информацию о сообществах.
+- Возможность подписки на авторов, чьи публикации вызывают интерес.
+- Редактирование возможно только в случае авторства (реализована проверка прав доступа).
+
+[![](https://img.shields.io/badge/Python-3.7.9-blue)](https://www.python.org/downloads/release/python-379/) [![](https://img.shields.io/badge/Django-3.2.16-green)](https://docs.djangoproject.com/en/4.1/releases/3.2.16/) [![](https://img.shields.io/badge/DRF-3.2.14-orange)](https://www.django-rest-framework.org/community/release-notes/#3124)
+
+#### Как запустить проект:
+1. Клонировать репозиторий и перейти в него в командной строке:
+```
+git clone https://github.com/A-Rogachev/api_final_yatube.git
+```
+```
+cd api_final_yatube
+```
+2. Создать и активировать виртуальное окружение.
+```
+python3 -m venv env
+```
+```
+source env/bin/activate
+```
+3. Установить зависимости из файла requirements.txt:
+```
+python3 -m pip install --upgrade pip
+```
+```
+pip install -r requirements.txt
+```
+4. Выполнить миграции:
+```
+python3 manage.py migrate
+```
+5. Запустить проект:
+ ```
+python3 manage.py runserver
+```
+#### *Документацию проекта, а также примеры запросов к API можно найти по следующему URL после запуска проекта:*
+```
+http://127.0.0.1:8000/redoc/
+```
+
+#### Автор
+
+><font size=2>Рогачев А.Г.  
+Студент факультета Бэкенд. Когорта № 50</font>
+
+[вверх](#start_page)
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -5,6 +5,8 @@ User = get_user_model()
 
 
 class Post(models.Model):
+    """Модель сообщения (публикация блога)."""
+
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
@@ -17,6 +19,8 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
+    """Модель комментария для публикации."""
+
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='comments')
     post = models.ForeignKey(
@@ -24,3 +28,66 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+
+class Follow(models.Model):
+    """Модель подписки."""
+
+    user = models.ForeignKey(
+        User,
+        verbose_name='Подписчик',
+        related_name='follower',
+        on_delete=models.CASCADE,
+    )
+
+    following = models.ForeignKey(
+        User,
+        verbose_name='Автор контента',
+        related_name='following',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'following'],
+                name='unique follow'
+            ),
+            models.CheckConstraint(
+                name='prevent_self_follow',
+                check=~models.Q(user=models.F('following')),
+            ),
+        ]
+
+    def __str__(self) -> str:
+        """Строковое представление подписки"""
+        return f'{self.user} подписан на {self.following}'
+
+
+class Group(models.Model):
+    """Модель сообщества."""
+
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название группы',
+        help_text='Укажите название группы',
+    )
+    slug = models.SlugField(
+        verbose_name='Slug для url-адреса',
+        help_text='Укажите slug для url-адреса группы',
+        unique=True,
+    )
+    description = models.TextField(
+        verbose_name='Описание группы',
+        help_text='Добавьте описание группы',
+    )
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+    def __str__(self) -> str:
+        """Возвращает название группы"""
+        return self.title
